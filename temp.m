94505d2511badbaa301587287571a775e27da0e9
Gimel-Donakins <fameusseamus@gmail.com>
2024-10-14 21:21:25 -0400

    SSC4 calculations

--- temp.m
+++ temp.m
@@ -1,52 +1,60 @@
 format compact
 format shortG
-airDensity = 1.293; % kg/m^3
-freshWaterDensity = 1000; % kg/m^3 
-saltWaterDensity = 1024; % kg/m^3
-EVAFoamDensity = 35; % kg/m^3
-PVCMaterialDensity = 1350; % kg/m^3
-PLAMaterialDensity = 1240; % kg/m^3
-gravelDensity = 1680; % kg/m^3
-gravity = 9.81; % m*s^-2
 
-% function BuoyantForce = BuoyantForce(density, gravity, volume)
-%     BuoyantForce =  density * gravity * volume;
-% end
+height = 0.22; % meters
+width = (1.2 - height)/2; % meters
+length = (1.2 - height)/2; % meters
+thicc = 0.00635; % meters (1/4 inch)
+[GM, percentSubmerged, Awet, HeelAngle, C, GradedValue] = metacentricHeight(width, height, length, thicc, 900);
 
-% avgMaterialDensity = (PVCMaterialDensity * 0.7) + (EVAFoamDensity * 0.3);
-% percentSubmerged = (avgMaterialDensity/freshWaterDensity);
+function [GM, percentSubmerged, Awet, HeelAngle, C, GradedValue] = metacentricHeight(width, height, length, thicc, materialDensity)
+    airDensity = 1.293; % kg/m^3
+    freshWaterDensity = 1000; % kg/m^3 
+    saltWaterDensity = 1024; % kg/m^3
+    EVAFoamDensity = 35; % kg/m^3
+    PVCMaterialDensity = 1350; % kg/m^3
+    PLAMaterialDensity = 1240; % kg/m^3
+    gravelDensity = 1680; % kg/m^3
+    gravity = 9.81; % m*s^-2
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Buoyancy Calculations for 32 oz. Quart size bag
+    GM = [];
+    Awet = [];
+    HeelAngle = [];
+    GradedValue = [];
 
-oneBagVolume = 0.000946353; % m^3
-oneBagMass = gravelDensity * oneBagVolume;
-disp("One bag weighs " + oneBagMass + "kg")
+    oneBagVolume = 0.000946353; % m^3
+    oneBagMass = gravelDensity * oneBagVolume;
 
-width = 0.4; % meters
-height = 0.2; % meters
-% length = 0.6*(1.2 - (width + height));
-length = 0.4; % meters
-thicc = 0.01; % meters
-Volume = width * height * length % whole enclosure m^3
-VolumeHull = width * height * length - ((width - thicc) * (height - thicc) * (length - thicc)) % m^3
-massHull = VolumeHull * 2300 + (Volume - VolumeHull)*airDensity
+    Volume = width * height * length; % whole enclosure m^3
+    VolumeHull = width * height * length - ((width - thicc) * (height - thicc) * (length - thicc)); % m^3
+    massHull = VolumeHull * materialDensity + (Volume - VolumeHull)*airDensity;
 
-CG = [0, 0, 0];
-G = CG + [width/2, length/2, height/2];
+    disp(VolumeHull*900)
 
-numberOfBags = 15
-weightedMass = massHull + numberOfBags*oneBagMass
-artificialDensity = weightedMass/Volume;
+    CG = [0, 0, 0];
+    G = CG + [width/2, length/2, height/2];
 
-percentSubmerged = (weightedMass/Volume)/freshWaterDensity
-submergedVolume = percentSubmerged*VolumeHull;
+    for numberOfBags = 0:1:100
+        C = numberOfBags*oneBagMass;
+        weightedMass = massHull + C;
+        percentSubmerged = (weightedMass/Volume)/freshWaterDensity;
+        submergedVolume = percentSubmerged*VolumeHull;
 
-T = percentSubmerged*height;
-KB = T/2;
-KG = G(3);
-IT = (2/3) * integral(@(x) (width/2).^3, -length/2, length/2, 'ArrayValued', true); % tranverse second moment of area
-BM = IT/submergedVolume; % metacentric radius
-GM = KB + BM - KG;
+        if percentSubmerged < 1
+            T = percentSubmerged*height;
+            KB = T/2;
+            KG = G(3);
+            IT = (2/3) * integral(@(x) (width/2).^3, -length/2, length/2, 'ArrayValued', true); % tranverse second moment of area
+            BM = IT/submergedVolume; % metacentric radius
+            GM = [GM, KB + BM - KG];
+            GM0 = GM(1);
+            Awet = [Awet, length*width + percentSubmerged*(2*height*width + 2*height*length)];
+            HeelAngle = [HeelAngle, atan((height - T)/(width/2))];
+            GradedValue = [GradedValue, (C*HeelAngle(numberOfBags+1)*GM(numberOfBags+1)*GM0)/Awet(numberOfBags+1)];
+        else
+            return;
+        end
+    end
+end
 
-disp("the value calculated for GM is " + GM + " meters") 
+maxGradedValue = max(GradedValue)
